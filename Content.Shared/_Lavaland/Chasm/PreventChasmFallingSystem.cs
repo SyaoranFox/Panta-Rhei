d038f102b1e5dbf8d74ffb239bf219f1cc43df5f
SyaoranFox <syaoranfox@gmail.com>
2026-04-03 15:42:04 +1100

    Most of Lavaland from Goob

--- Content.Shared/_Lavaland/Chasm/PreventChasmFallingSystem.cs
+++ Content.Shared/_Lavaland/Chasm/PreventChasmFallingSystem.cs
@@ -24,7 +24,6 @@ using Content.Shared.Chasm;
 using Content.Shared.Interaction;
 using Content.Shared.Inventory;
 using Content.Shared.Timing;
-using Robust.Shared.Audio;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Containers;
 using Robust.Shared.Map;
@@ -40,8 +39,6 @@ public sealed class PreventChasmFallingSystem : EntitySystem
     [Dependency] private readonly SharedInteractionSystem _interaction = default!;
     [Dependency] private readonly EntityLookupSystem _lookup = default!;
     [Dependency] private readonly UseDelaySystem _delay = default!;
-
-    private static readonly ResolvedPathSpecifier FallSound = new("/Audio/Items/Mining/fultext_launch.ogg"); // DeltaV - Avoid literal
 
     public override void Initialize()
     {
@@ -75,7 +72,7 @@ public sealed class PreventChasmFallingSystem : EntitySystem
 
             _transform.SetCoordinates(args.Entity, newCoords);
             _transform.AttachToGridOrMap(args.Entity, Transform(args.Entity));
-            _audio.PlayPvs(FallSound, args.Entity); // DeltaV - Avoid literal
+            _audio.PlayPvs("/Audio/Items/Mining/fultext_launch.ogg", args.Entity);
             if (args.Entity != uid && comp.DeleteOnUse)
                 QueueDel(uid);
             else if (useDelay != null)
